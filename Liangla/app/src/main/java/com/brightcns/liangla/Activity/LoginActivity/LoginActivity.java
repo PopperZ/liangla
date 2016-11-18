88dca4284a0f069af2fe8745321d5289dcf5b672
wugang <wugang@gengbeibeis-Mac-mini.local>
2016-11-18 18:51:30 +0800

    no message

---
+++ Liangla/app/src/main/java/com/brightcns/liangla/Activity/LoginActivity/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.brightcns.liangla.Activity.LoginActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.brightcns.liangla.Activity.RegisterActivity.RegisterActivity;
+import com.brightcns.liangla.R;
+
+public class LoginActivity extends Activity implements View.OnClickListener{
+    private Button mRegister,mResetPwd,mLogin;
+    private EditText mPhoneNum,mPwd;
+    private ImageButton mPwd_show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+    private void init(){
+        mPhoneNum   = (EditText) findViewById(R.id.phonenum);
+        mPhoneNum.setOnClickListener(this);
+        mPwd        = (EditText) findViewById(R.id.pwd);
+        mPwd.setOnClickListener(this);
+        mRegister   = (Button) findViewById(R.id.register);
+        mRegister.setOnClickListener(this);
+        mResetPwd   = (Button) findViewById(R.id.reset_pwd);
+        mResetPwd.setOnClickListener(this);
+        mLogin      = (Button) findViewById(R.id.login);
+        mLogin.setOnClickListener(this);
+        mPwd_show   = (ImageButton) findViewById(R.id.pwd_show);
+        mPwd_show.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                Intent register=new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(register);
+                break;
+            case R.id.reset_pwd:
+                Intent resetpwd=new Intent(LoginActivity.this, ResetPwdActivity.class);
+                startActivity(resetpwd);
+                break;
+            case R.id.login:
+                Toast.makeText(this,"正在登陆。。。。",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pwd_show:
+
+                Toast.makeText(this,"显示密码",Toast.LENGTH_SHORT).show();
+
+                break;
+
+        }
+    }
+}
--- Liangla/app/src/main/java/com/brightcns/liangla/fragment/PerCenterFragment.java
+++ Liangla/app/src/main/java/com/brightcns/liangla/fragment/PerCenterFragment.java
@@ -1,11 +1,14 @@
 package com.brightcns.liangla.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.brightcns.liangla.Activity.LoginActivity.LoginActivity;
 import com.brightcns.liangla.R;
 
 /**
@@ -14,12 +17,21 @@ import com.brightcns.liangla.R;
 
 public class PerCenterFragment extends Fragment{
     private View view;// 需要返回的布局
+    private LinearLayout mLogin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (view == null) {// 优化View减少View的创建次数
             view = inflater.inflate(R.layout.percenter_layout, null);
+            mLogin= (LinearLayout) view.findViewById(R.id.no_login);
+            mLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent =new Intent(getContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            });
             return view;
         }
         return view;
